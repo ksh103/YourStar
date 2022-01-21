f8f001eeb5fc9e88e6405d9ad1c0b0336594e657
지수민 <wltn1873@gmail.com>
2022-01-21 10:08:43 +0900

    Update Navbar UI

--- Frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -1,69 +1,113 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import {
-  Logo,
+  CenterMenu,
   NavbarWrapper,
   LeftMenu,
   RightMenu,
   GridBlock,
 } from './Navbar.style';
 import logo from './yourstar_logo.png';
-import Grid from '@mui/material/Grid';
-
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
 export default function Navbar() {
   const [open, setOpen] = useState(false);
-  let role = 'unlogin';
-  let isOpen = false;
-  const openMenu = () => {
-    setOpen(true);
+  let role = 1;
+  const toggleDrawer = open => event => {
+    if (
+      event.type === 'keydown' &&
+      (event.key === 'Tab' || event.key === 'Shift')
+    )
+      return;
+    setOpen(open);
   };
+  const list = () => (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        <ListItem>
+          <Link to="login">Login</Link>
+        </ListItem>
+        <ListItem>
+          <Link to="login">meeting</Link>
+        </ListItem>
+        <ListItem>
+          <Link to="login">FAQ</Link>
+        </ListItem>
+        <ListItem>
+          <Link to="login">contact</Link>
+        </ListItem>
+        <ListItem>
+          <Link to="login">mypage</Link>
+        </ListItem>
+      </List>
+    </Box>
+  );
+
   return (
     <div>
       <NavbarWrapper>
         <GridBlock xs={4}>
-          <LeftMenu onClick={openMenu}>햄버거아이콘</LeftMenu>
+          <LeftMenu>
+            <div onClick={toggleDrawer(true)}>⬜</div>
+            <React.Fragment key={'left'}>
+              <Drawer anchor={'left'} open={open} onClose={toggleDrawer(false)}>
+                {list()}
+              </Drawer>
+            </React.Fragment>
+          </LeftMenu>
         </GridBlock>
         <GridBlock xs={4}>
-          <Logo>
-            <img src={logo} alt="yourstar" />
-          </Logo>
+          <CenterMenu>
+            <Link to="/">
+              <img src={logo} alt="yourstar" />
+            </Link>
+          </CenterMenu>
         </GridBlock>
         <GridBlock xs={4}>
-          <RightMenu>각종 메뉴</RightMenu>
+          <RightMenu>
+            <ul>
+              {role === 0 ? (
+                <li>
+                  <Link to="login">Login</Link>
+                </li>
+              ) : (
+                <>
+                  <li>
+                    <Link to="mypage">🧞‍♂️ 지수민 님</Link>
+                  </li>
+                  <li>
+                    <div>Logout</div>
+                  </li>
+                </>
+              )}
+
+              <li>
+                <Link to="faq">FAQ</Link>
+              </li>
+              <li>
+                <Link to="schedule">Meeting</Link>
+              </li>
+              {role === 2 && (
+                <li>
+                  <Link to="Apply">contact</Link>
+                </li>
+              )}
+              {role === 3 && (
+                <li>
+                  <Link to="Admin">Management</Link>
+                </li>
+              )}
+            </ul>
+          </RightMenu>
         </GridBlock>
       </NavbarWrapper>
-
-      {/* mobile 일때 보이는 메뉴창 */}
-      {/* <ul>
-        <li>
-          <Link to="/">
-            <div>
-              <img src={yourstar_logo} alt="yourstar" />
-            </div>
-          </Link>
-        </li>
-        <li>
-          <Link to="login">로그인</Link>
-        </li>
-        <li>
-          <Link to="signup">회원가입</Link>
-        </li>
-        <li>
-          <Link to="faq">FAQ</Link>
-        </li>
-        <li>
-          <Link to="schedule">일정</Link>
-        </li>
-        <li>
-          <Link to="apply">미팅신청</Link>
-        </li>
-        <li>
-          <Link to="admin">미팅관리</Link>
-        </li>
-        <li>
-          <Link to="mypage">마이페이지</Link>
-        </li>
-      </ul> */}
     </div>
   );
 }
--- Frontend/src/components/Navbar/Navbar.style.js
+++ Frontend/src/components/Navbar/Navbar.style.js
@@ -1,45 +1,51 @@
 import styled from 'styled-components';
 import Grid from '@mui/material/Grid';
-import { device } from '../../styles/variables';
+//import { device } from '../../styles/variables';
 
 const NavbarWrapper = styled.div`
-  border: 2px solid blue;
+  padding: 20px 40px;
   display: flex;
   justify-content: space-between;
   font-size: 22px;
+  @media (max-width: 500px) {
+    padding: 15px;
+  }
 `;
 const LeftMenu = styled.div`
+  width: 100%;
   display: none;
   @media (max-width: 500px) {
-    display: block;
+    display: flex;
   }
 `;
 const RightMenu = styled.div`
+  width: 100%;
+  font-size: 17px;
   display: block;
+  ul li {
+    list-style: none;
+    float: right;
+    margin: 0 15px;
+  }
+
   @media (max-width: 500px) {
     display: none;
   }
 `;
 
-const Logo = styled.div`
-  border: 2px solid red;
+const CenterMenu = styled.div`
   display: inline-block;
   img {
     width: 230px;
   }
   @media (max-width: 480px) {
     img {
-      width: 150px;
+      width: 180px;
     }
   }
 `;
 const GridBlock = styled(Grid)`
-  border: 2px solid white;
   display: flex;
   justify-content: space-around;
 `;
-export { NavbarWrapper, Logo, LeftMenu, RightMenu, GridBlock };
-
-// flex-start
-// flex-end
-// center
+export { NavbarWrapper, CenterMenu, LeftMenu, RightMenu, GridBlock };
