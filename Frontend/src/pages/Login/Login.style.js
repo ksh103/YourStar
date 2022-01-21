965a16789abd54907d369a6e557acae85b80895d
박동준 <qkrehdwns96@naver.com>
2022-01-21 16:01:18 +0900

    [S06P12E204-234] UI디자인-FE : 회원관리-회원가입창 Color theme 적용 & 사진 제거

--- Frontend/src/pages/Login/Login.style.js
+++ Frontend/src/pages/Login/Login.style.js
@@ -1,15 +1,45 @@
 import styled from 'styled-components';
 
 const innerDiv = styled.div`
-  height : 100%
-`
+  height: 100%;
+`;
 
 const ImgTag = styled.img`
-  object-fit: cover;;
-  height : 99.5%;
-  width : 100%;
+  object-fit: cover;
+  height: 99.5%;
+  width: 100%;
   border-radius: 30px;
   border: 2px solid white;
-`
+`;
 
-export {innerDiv,ImgTag}
+const size = {
+  MobileLandscape: '480px',
+  TabletPortrait: '768px',
+  TabletLandscape: '992px',
+  Laptops: '1200px',
+};
+
+export const device = {
+  MobileLandscape: `(min-width: ${size.MobileLandscape})`,
+  TabletPortrait: `(min-width: ${size.TabletPortrait})`,
+  TabletLandscape: `(min-width: ${size.TabletLandscape})`,
+  Laptops: `(min-width: ${size.Laptops})`,
+};
+
+const LoginSignupBlock = styled.div`
+  //border: 1px solid red;
+  margin-top: 50px;
+  margin-bottom: 50px;
+  margin-left: 60vh;
+  margin-right: 60vh;
+  background-color: white;
+  height: 90vh;
+  max-height: 100%;
+  border-radius: 30px;
+  text-align: center;
+  /* width: 100vw; */
+  @media ${device.MobileLandscape} {
+  }
+`;
+
+export { innerDiv, ImgTag, LoginSignupBlock };
