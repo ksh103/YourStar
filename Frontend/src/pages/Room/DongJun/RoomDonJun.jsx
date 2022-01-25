938e094d335a08ff29f949d1c08b649f3a3a263d
손은성 <dmstjd3256@naver.com>
2022-01-25 10:41:46 +0900

    [S06P12E204-6] FE-미팅룸: Component test폴더 업로드

---
+++ Frontend/src/pages/Room/DongJun/RoomDonJun.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function RoomDonJun() {
+  return <div></div>;
+}
---
+++ Frontend/src/pages/Room/Sumin/RoomSumin.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function RoomSumin() {
+  return <div></div>;
+}
