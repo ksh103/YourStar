059b11a68483cee29e268eac9289f6ce334b2d68
박동준 <qkrehdwns96@naver.com>
2022-01-26 11:49:37 +0900

    [S06P12E204-265] FE-UI디자인: 미팅룸 공통 컴포넌트 생성 완료

---
+++ Frontend/src/pages/Room/DongJun/EmoziBar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+import Emozi from './Emozi';
+
+const EmoziCompo = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  width: 60.1416vw;
+  height: 8vh;
+  background-color: white;
+  border-radius: 4vh;
+  box-shadow: 0.306vh 0.306vh gray;
+`;
+
+const EmoziPs = styled.div`
+  position: absolute;
+  top: 83%;
+  left: 20%;
+`;
+
+export default function EmoziBar() {
+  return (
+    <EmoziPs>
+      <EmoziCompo>
+        <Emozi></Emozi>
+      </EmoziCompo>
+    </EmoziPs>
+  );
+}
