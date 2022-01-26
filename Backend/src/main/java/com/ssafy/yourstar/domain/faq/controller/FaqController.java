fa701faa1bb9032c1979b511809a7ffe0f471252
안영원 <kublaihkan@gmail.com>
2022-01-26 17:31:59 +0900

    bugfix-BE : FAQ 전체 조회 수정

--- Backend/src/main/java/com/ssafy/yourstar/domain/faq/controller/FaqController.java
+++ Backend/src/main/java/com/ssafy/yourstar/domain/faq/controller/FaqController.java
@@ -6,13 +6,12 @@ import com.ssafy.yourstar.domain.faq.service.FaqService;
 import com.ssafy.yourstar.global.model.response.BaseResponseBody;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 
 @Slf4j
@@ -26,7 +25,7 @@ public class FaqController {
 
     @ApiOperation(value = "FAQ 등록")
     @PostMapping
-    public ResponseEntity<BaseResponseBody> faqRegister(@RequestBody FaqRegisterPostReq faqRegister, HttpServletRequest request) {
+    public ResponseEntity<BaseResponseBody> faqRegister(@RequestBody FaqRegisterPostReq faqRegister) {
         log.info("faqRegister - 호출");
         faqService.faqRegister(faqRegister);
 
@@ -35,14 +34,14 @@ public class FaqController {
 
     @ApiOperation(value = "FAQ 전체 조회")
     @GetMapping
-    public List<Faq> faqList() {
+    public Page<Faq> faqList(@ApiParam(value = "페이지 번호")@RequestParam int page, @ApiParam(value = "페이지당 게시글 개수") @RequestParam int size) {
         log.info("faqList - 호출");
-        return faqService.faqList();
+        return faqService.faqList(page, size);
     }
 
     @ApiOperation(value = "FAQ 수정")
     @PutMapping("/{faqId}")
-    public ResponseEntity<BaseResponseBody> faqModify(@RequestBody Faq faq, HttpServletRequest request) {
+    public ResponseEntity<BaseResponseBody> faqModify(@RequestBody Faq faq) {
         log.info("faqModify - 호출");
         if (faqService.faqModify(faq) == null) {    // 해당 FAQ 게시물 존재하지 않는 경우
             log.error("faqModify - This faqId doesn't exist.");
@@ -54,7 +53,7 @@ public class FaqController {
 
     @ApiOperation(value = "FAQ 삭제")
     @DeleteMapping("/{faqId}")
-    public ResponseEntity<BaseResponseBody> faqRemove(@PathVariable int faqId, HttpServletRequest request) {
+    public ResponseEntity<BaseResponseBody> faqRemove(@PathVariable int faqId) {
         log.info("faqRemove - 호출");
         if (faqService.faqRemove(faqId)) {  // 정상 작동
             return ResponseEntity.status(201).body(BaseResponseBody.of(201, "Success"));
