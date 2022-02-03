8d817649d551931af6b24d3a9d0434e84022ecfd
손은성 <dmstjd3256@naver.com>
2022-02-03 16:06:52 +0900

    [S06P12E204-288] FE-Redux: 마이페이지 api, modules 수정

--- Frontend/src/store/modules/meeting.js
+++ Frontend/src/store/modules/meeting.js
@@ -1,4 +1,24 @@
 import produce from 'immer';
-const initialState = {};
-const reducer = (state = initialState, action) => produce(state, draft => {});
+const initialState = {
+  myPageLoading: false, // 마이페이지 정보
+  myPageDone: false,
+  myPageError: false,
+};
+
+export const MY_PAGE_REQUEST = 'MY_PAGE_REQUEST';
+export const MY_PAGE_SUCCESS = 'MY_PAGE_SUCCESS';
+export const MY_PAGE_FAILURE = 'MY_PAGE_FAILURE';
+
+const reducer = (state = initialState, action) =>
+  produce(state, draft => {
+    switch (action.type) {
+      case MY_PAGE_REQUEST:
+        draft.myPageLoading = true;
+        draft.myPageError = null;
+        draft.myPageDone = false;
+        break;
+      default:
+        break;
+    }
+  });
 export default reducer;
