4fb21af00f815bb17f541a1f1b0603d360d053f2
박동준 <qkrehdwns96@naver.com>
2022-02-03 15:04:26 +0900

    [S06P12E204-261] FE-미팅룸UI: QnA상호작용 fix

---
+++ Frontend/src/pages/Room/DongJun/RoomDonJun copy.jsx
@@ -0,0 +1,107 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import StarConsonantGame from '../../../components/room/Game/ConsonantGame/StarConsonantGame';
+import Warning from '../../../components/room/CommonComponents/Alert/Warning';
+
+const StickBar = styled.div`
+  width: 60.1416vw;
+  height: 5.517vh;
+  background-color: white;
+  border-radius: 3vh;
+  box-shadow: 0.306vh 0.306vh gray;
+`;
+
+const StickBarDiv = styled.div`
+  position: absolute;
+  top: 65.5%;
+  left: 8%;
+`;
+
+// 잠시 확인용 삭제가능
+const StickBarUserDiv = styled.div`
+  position: absolute;
+  top: 50.5%;
+  left: 8%;
+`;
+
+const GridDiv = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  margin-left: 2vw;
+  color: black;
+`;
+
+const InnerDiv = styled.button`
+  margin: 1vw;
+  color: black;
+`;
+
+const UserInput = styled.input`
+  outlint: 0.3vw solid black;
+  border-color: black;
+  width: 40vw;
+  height: 4vh;
+  border-radius: 3vh;
+  padding-left: 0.5vw;
+  margin: 1vw;
+`;
+
+const RoomDonJun = () => {
+  const [QnAMode, setQnAMode] = useState(0);
+  const [userId, setUserId] = useState(1);
+  const [QnAText, setQnAText] = useState('');
+  const [QnAList, setQnAList] = useState([]);
+  const [submitFlag, setsubmitFlag] = useState(false);
+
+  // useEffect
+  useEffect(() => {
+    console.log('제출상황');
+    console.log(submitFlag);
+  }, [submitFlag]);
+  // 0 : user, 1 : star
+  const qnaStart = e => {
+    setQnAMode(e);
+    console.log(e, '모드변경');
+    if (e === 0) {
+      //모든 유저상태 변경을 시켜 주어야함!
+      setsubmitFlag(false);
+    } else if (e === 1) {
+      setsubmitFlag(true);
+    } else {
+      console.log('모드를 스티커가 보이게 변경시켜주어야함');
+    }
+  };
+
+  const submitQnA = e => {
+    e.preventDefault();
+    setQnAList([...QnAList, e.target[0].value]);
+    setsubmitFlag(true);
+    setQnAText('');
+  };
+
+  const valueChange = e => {
+    console.log(e.target.value);
+    setQnAText(e.target.value);
+  };
+
+  if (userId === 1) {
+    return (
+      <div
+        style={{
+          position: 'absolute',
+          width: '100%',
+          backgroundColor: '#E2D8FF',
+          height: '100%',
+          backgroundImage: 'none',
+        }}
+      >
+        <Warning></Warning>
+      </div>
+    );
+  }
+};
+
+export default RoomDonJun;
