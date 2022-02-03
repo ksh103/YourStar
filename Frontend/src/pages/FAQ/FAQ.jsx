f6051254099c13159252ab19ea9f2869d83f7058
지수민 <wltn1873@gmail.com>
2022-02-03 16:05:47 +0900

    [S06P12E204-289] Redux상태관리-FE : faq 연결

--- Frontend/src/pages/FAQ/FAQ.jsx
+++ Frontend/src/pages/FAQ/FAQ.jsx
@@ -7,7 +7,8 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import { MdExpandMore } from 'react-icons/md';
 import Footer from '../../components/Footer/Footer.jsx';
-import axios from 'axios';
+import { LOAD_FAQS_REQUEST } from '../../store/modules/faq';
+import { useDispatch, useSelector } from 'react-redux';
 const datas = [
   {
     id: 1,
@@ -45,42 +46,48 @@ const datas = [
     content: '아니요. 래퍼 쌈디입니다.',
   },
 ];
-const FAQDate = () => {
-  const check = () => {
-    const a = axios
-      .get('http://i6e204.p.ssafy.io:8080/api/faq?page=1&size=5')
-      .then(res => res.status === 200 && console.log(res));
-  };
-  check();
 
-  return datas.map(faq => (
-    <FAQContentRow key={faq.id}>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<MdExpandMore />}
-          sx={{ fontSize: '1.1em' }}
-        >
-          <div>{faq.title}</div>
-        </AccordionSummary>
-        <AccordionDetails sx={{ borderTop: `2px solid ${blockColor}` }}>
-          <div>{faq.content}</div>
-        </AccordionDetails>
-      </Accordion>
-    </FAQContentRow>
-  ));
-};
 export default function FAQ() {
+  const dispatch = useDispatch();
+  const { faqs, loadFaqsDone } = useSelector(state => state.faq);
+  useEffect(() => {
+    dispatch({
+      type: LOAD_FAQS_REQUEST,
+      data: { page: 1, size: 10 },
+    });
+  }, [dispatch]);
+  const FAQDate = () => {
+    return faqs.map(faq => (
+      <FAQContentRow key={faq.id}>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<MdExpandMore />}
+            sx={{ fontSize: '1.1em' }}
+          >
+            <div>{faq.faqTitle}</div>
+          </AccordionSummary>
+          <AccordionDetails sx={{ borderTop: `2px solid ${blockColor}` }}>
+            <div>{faq.faqContent}</div>
+          </AccordionDetails>
+        </Accordion>
+      </FAQContentRow>
+    ));
+  };
   return (
     <Layout>
       <Navbar />
       <Wrapper>
         <Block>
-          <FAQHeader>자주 묻는 질문</FAQHeader>
-          <FAQContent>
-            <div id="faq">
-              <div id="faq2">{FAQDate()}</div>
-            </div>
-          </FAQContent>
+          {loadFaqsDone && (
+            <>
+              <FAQHeader>자주 묻는 질문</FAQHeader>
+              <FAQContent>
+                <div id="faq">
+                  <div id="faq2">{FAQDate()}</div>
+                </div>
+              </FAQContent>
+            </>
+          )}
         </Block>
       </Wrapper>
       <Footer />
