e6c4bd82adfc2fe4ed72c1f352b7a79341bd602d
안영원 <kublaihkan@gmail.com>
2022-02-07 17:39:55 +0900

    [S06P12E204-365] MeetngRoom-BE : 이미지 경로 수정 및 로그 추가
    리눅스 기준으로 이미지 경로가 작성되어 있음

--- Backend/src/main/java/com/ssafy/yourstar/domain/meeting/service/MeetingService.java
+++ Backend/src/main/java/com/ssafy/yourstar/domain/meeting/service/MeetingService.java
@@ -4,15 +4,16 @@ import com.ssafy.yourstar.domain.meeting.db.entity.Applicant;
 import com.ssafy.yourstar.domain.meeting.db.entity.Meeting;
 import com.ssafy.yourstar.domain.meeting.request.MeetingApplyByStarPostReq;
 import com.ssafy.yourstar.domain.meeting.request.MeetingApplyByUserPostReq;
-import com.ssafy.yourstar.domain.meeting.response.MeetingGetRes;
 import com.ssafy.yourstar.domain.member.db.entity.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import java.io.IOException;
+
 public interface MeetingService {
-    MeetingGetRes meetingApplyByStar(MeetingApplyByStarPostReq meetingApplyByStarPostReq, MultipartHttpServletRequest request);
-    MeetingGetRes meetingModifyByStar(Meeting meeting, MultipartHttpServletRequest request);
+    int meetingApplyByStar(MeetingApplyByStarPostReq meetingApplyByStarPostReq, MultipartHttpServletRequest request) throws IOException;
+    int meetingModifyByStar(Meeting meeting, MultipartHttpServletRequest request) throws IOException;
     boolean meetingRemoveByStar(int meetingId);
     Page<Meeting> meetingList(Pageable pageable);
     Page<Meeting> meetingPendingList(Pageable pageable);
