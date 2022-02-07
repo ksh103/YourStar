4a6af7cb568cae9e680e5fa4240b9a6fd792c43d
강소현 <jayy_z@naver.com>
2022-02-08 01:59:47 +0900

    [S06P12E204-362] 추억보관함-BE : 팬미팅 스크린샷 다운로드 기능 구현

--- Backend/src/main/java/com/ssafy/yourstar/domain/meeting/service/MeetingRecordService.java
+++ Backend/src/main/java/com/ssafy/yourstar/domain/meeting/service/MeetingRecordService.java
@@ -1,10 +1,14 @@
 package com.ssafy.yourstar.domain.meeting.service;
 
 import com.ssafy.yourstar.domain.meeting.db.entity.Meeting;
+import com.ssafy.yourstar.domain.meeting.db.entity.MeetingRecordImgPath;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 public interface MeetingRecordService {
 
     Page<Meeting> meetingRecordList(int memberId, Pageable pageable);
+    List<MeetingRecordImgPath> meetingRecordImgDetail(int meetingId, int memberId);
 }
