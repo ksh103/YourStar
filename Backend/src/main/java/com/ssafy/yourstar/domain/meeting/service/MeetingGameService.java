0d5f69e4bc02ae785a2ab6ba96fdcbf35f43ab3d
강소현 <jayy_z@naver.com>
2022-02-11 15:52:13 +0900

    [S06P12E204-398] 미팅룸게임-BE : 미팅룸 게임 결과에 대한 점수 반환 API 기능 구현

--- Backend/src/main/java/com/ssafy/yourstar/domain/meeting/service/MeetingGameService.java
+++ Backend/src/main/java/com/ssafy/yourstar/domain/meeting/service/MeetingGameService.java
@@ -6,6 +6,8 @@ import com.ssafy.yourstar.domain.meeting.request.MeetingGameWinnerApplyByUserPos
 import java.util.List;
 
 public interface MeetingGameService {
+    boolean meetingGameScore(int meetingId, int memberId);
+
     List<String> meetingGameResultListByUser(int memberId); // 게임 우승자 내역 (회원 확인용)
     List<String> meetingGameResultListByStar(int meetingId); // 게임 우승자 내역 (스타 확인용)
     MeetingGame meetingGameWinnerApply(MeetingGameWinnerApplyByUserPostReq meetingGameWinnerApplyByUserPostReq); // 게임 우승자 등록
