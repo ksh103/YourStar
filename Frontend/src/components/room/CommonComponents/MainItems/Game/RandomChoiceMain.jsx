cd2deee28e82b370a6c943042bedeb09a66b81ed
박동준 <qkrehdwns96@naver.com>
2022-02-13 10:34:34 +0900

    [S06P12E204-414] FE-OpenVidu:랜덤게임 롤링

--- Frontend/src/components/room/CommonComponents/MainItems/Game/RandomChoiceMain.jsx
+++ Frontend/src/components/room/CommonComponents/MainItems/Game/RandomChoiceMain.jsx
@@ -1,11 +1,29 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { MainDiv } from '../Main.style';
 import { useSelector } from 'react-redux';
 import StarVideoComponent from '../../../../../pages/Room/StarVideoComponent';
-import swal from 'sweetalert';
 import './alertCss.css';
 import UserVideoComponent from '../../../../../pages/Room/UserVideoComponent';
+import swal from '@sweetalert/with-react';
+import './rolling.scss';
+
+const RollingDiv = styled.div`
+  width: 20rem;
+  display: inline-block;
+  position: absolute;
+  top: -200px;
+  transform: rotateX(-90deg);
+  opacity: 0;
+  text-shadow: 0px 5px 5px rgba(0, 0, 0, 0.25);
+  animation-timing-function: ease;
+  &: nth-child(${num});
+   {
+    animation: rollDown 3s 1;
+  }
+``
+`;
+
 // 60vw 가로폭
 const RandomChoiceSc = styled.div`
   position: relative;
@@ -15,11 +33,6 @@ const RandomChoiceSc = styled.div`
   border-radius: 3.0643vh;
   box-shadow: 0.306vh 0.306vh gray;
   margin-right: 4vw;
-`;
-
-const MId = styled.div`
-  width
-
 `;
 
 const MainGrid = styled.div`
@@ -40,20 +53,14 @@ const UserList = [
   '공혁준',
   '육지담',
 ];
-//git commit -m "[S06P12E204-261] FE-미팅룸UI: RandomGame생성&미팅룸redux 생성 &  "
-const shuffleArray = array => {
-  for (let i = 0; i < array.length; i++) {
-    let j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-};
 
+//git commit -m "[S06P12E204-261] FE-미팅룸UI: RandomGame생성&미팅룸redux 생성 &  "
 export default function RandomChoiceMain() {
+  const [test, setTest] = useState(false);
   const [result, setResult] = useState(''); // 변경점
   // 랜덤으로 뽑힌 유저의 정보를 모두에게 적용시켜야함
   const [randomUser, setRandomUser] = useState('');
-
+  const [num, setNum] = useState(0);
   // store 정보 불러오기
   const { me } = useSelector(state => state.mypage);
   const { mainStreamManager } = useSelector(state => ({
@@ -61,14 +68,43 @@ export default function RandomChoiceMain() {
   }));
   // 유저정보 불러오기 --> 랜덤으로 하나를 뽑기 위함
   const { subscribers, storeSession } = useSelector(state => state.MeetingRoom);
+  const [pickone, setpickone] = useState('지슬');
+  const [studentsName, setstudentsName] = useState([
+    '동준',
+    '지슬',
+    '은성',
+    '수민',
+    '영원',
+    '소현',
+    '쌈디',
+  ]);
+  console.log('⭐⭐⭐⭐픽원 : ', pickone);
+  const [sec, setSec] = useState(5);
+  const time = useRef(5);
+  const timerId = useRef(null);
+  useEffect(() => {
+    timerId.current = setInterval(() => {
+      setSec(time.current % 60);
+      time.current -= 1;
+    }, 1000);
+    return () => clearInterval(timerId.current);
+  }, []);
 
+  useEffect(() => {
+    if (time.current < 0) {
+      // 만약 타임 아웃이 발생했을 경우
+
+      clearInterval(timerId.current);
+    }
+  }, [sec]);
+  let i = 0;
   const onShuffle = e => {
     console.log(e);
     // 아래 리스트만 바뀐 로직은 두세요 추데이터 받아왔을때 실험합니다.
     // const rand = Math.floor(Math.random() * subscribers.length);
     const rand = Math.floor(Math.random() * UserList.length);
     // const randomresult = subscribers[rand];
-    const randomresult = subscribers[rand];
+    const randomresult = UserList[rand];
     setResult(randomresult);
     storeSession.signal({
       data: randomresult,
@@ -79,40 +115,39 @@ export default function RandomChoiceMain() {
 
   storeSession.on('signal:random', event => {
     console.log(event, '========랜덤정보 수신');
-
-    setTimeout(function () {
-      swal('3', {
-        buttons: false,
-        timer: 500,
-      }); // 틀렸을 때 게임 다시하기위해 호출하는 함수
+    swal({
+      buttons: false,
+      timer: 5000,
+      className: '.countDown',
+      content: (
+        <div>
+          {pickone !== undefined && (
+            <>
+              <div className="rolling">
+                <div className="slider">
+                  <div className="caption">
+                    행운의 주인공은?!
+                    <div className="text-box">
+                      <div>{studentsName[i]}</div>
+                      <div>{studentsName[(i + 1) % studentsName.length]}</div>
+                      <div>{studentsName[(i + 2) % studentsName.length]}</div>
+                      <div>{studentsName[(i + 3) % studentsName.length]}</div>
+                      <div>{studentsName[(i + 4) % studentsName.length]}</div>
+                      <div>{studentsName[(i + 5) % studentsName.length]}</div>
+                      {/* <div>{studentsName[(i + 6) % studentsName.length]}</div>
+                      <div>{studentsName[(i + 7) % studentsName.length]}</div>
+                      <div>{studentsName[(i + 8) % studentsName.length]}</div> */}
+                      <div>{pickone}</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </>
+          )}
+        </div>
+      ),
     });
-    setTimeout(function () {
-      swal('2', {
-        buttons: false,
-        timer: 500,
-      }); // 틀렸을 때 게임 다시하기위해 호출하는 함수
-    }, 1000);
-    setTimeout(function () {
-      swal('1', {
-        buttons: false,
-        timer: 500,
-      }); // 틀렸을 때 게임 다시하기위해 호출하는 함수
-    }, 2000);
-    // 유저컴포정보 띄워주기 !!!
-    setTimeout(setRandomUser(event), 3000);
   });
-
-  // 모달에 시간별로 사진 or 텍스트 띄우기
-  // const CountAlert = () => {
-  //   swal("Here's the title!", "...and here's the text!", {
-  //     buttons: false,
-  //     timer: 3000,
-  //   });
-  // };
-  // let arr = [3, 2, 1];
-  // const countDown = arr.forEach((value, idx) => {
-  //   return alert(value, { button: false, timer: 1000 });
-  // });
 
   return (
     <MainDiv>
