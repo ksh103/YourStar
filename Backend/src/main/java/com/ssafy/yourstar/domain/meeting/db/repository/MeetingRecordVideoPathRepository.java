be876b3da000910f84abee7f9737bc5af541de74
강소현 <jayy_z@naver.com>
2022-02-14 17:46:51 +0900

    [S06P12E204-432] 미팅룸관리-BE : 미팅룸 녹화 영상 다운로드

--- Backend/src/main/java/com/ssafy/yourstar/domain/meeting/db/repository/MeetingRecordVideoPathRepository.java
+++ Backend/src/main/java/com/ssafy/yourstar/domain/meeting/db/repository/MeetingRecordVideoPathRepository.java
@@ -2,8 +2,12 @@ package com.ssafy.yourstar.domain.meeting.db.repository;
 
 import com.ssafy.yourstar.domain.meeting.db.entity.MeetingRecordVideoPath;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MeetingRecordVideoPathRepository extends JpaRepository<MeetingRecordVideoPath, Integer> {
+
+    @Query("select m.fileUrl from MeetingRecordVideoPath m where m.meetingId = :meetingId and m.memberId = :memberId")
+    String findFileUrlByMeetingIdAndMemberId(int meetingId, int memberId);
 }
--- Backend/src/main/java/com/ssafy/yourstar/domain/meeting/service/MeetingRecordService.java
+++ Backend/src/main/java/com/ssafy/yourstar/domain/meeting/service/MeetingRecordService.java
@@ -14,5 +14,6 @@ public interface MeetingRecordService {
     Page<Meeting> meetingRecordList(int memberId, Pageable pageable); // 추억 보관함 목록
     int meetingRecordImgSave(MeetingRecordImgPathPostReq meetingRecordImgPathPostReq) throws IOException;
     List<MeetingRecordImgPath> meetingRecordImgDetail(int meetingId, int memberId); // 추억 보관함 사진 다운로드
+    String meetingRecordVideoFileUrl(int meetingId, int memberId);
     int meetingRecordImgRemove(int fileId); // 추억 보관함 사진 개별 삭제
 }
